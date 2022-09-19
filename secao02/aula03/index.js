e1207de9ec678d83fb83cbff0292d451a03e43c8
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-09-19 18:45:00 -0300

    Primeira aula

---
+++ secao02/aula03/index.js
@@ -0,0 +1,12 @@
+//Aula - Console.log
+
+console.log("Gabriel 'Neves'");
+console.log('Gabriel "N"');
+console.log(`'Gabriel' "Pieroni"`);
+
+console.log(35, 15.85, 'Gabriel Neves');
+
+//Tipos de Dados
+
+//String = Texto
+//Number = Número
