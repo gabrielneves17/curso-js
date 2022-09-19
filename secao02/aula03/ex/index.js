34831d438973effba8c55573202512b617985e19
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-09-19 20:45:19 -0300

    Exercício e Aulas - 19/09

---
+++ secao02/aula03/ex/index.js
@@ -0,0 +1,3 @@
+//Exercício - Separando strings de números literais com aspas simples
+
+console.log('Meu nome é "GABRIEL". Estou aprendendo Javascript às', 10, 'horas da manhã.');
---
+++ secao02/aula04/comentarios.js
@@ -0,0 +1,11 @@
+//Comentários - Trechos de código ignorados pelo motor do Javascript. Servem de anotações sobre códigos escritos.
+
+//Exemplo
+//ESSE -> TEXTO NÃO SERÁ EXIBIDO
+console.log('Olá mundo'); //Aqui tem um comentário
+console.log('Linha 1');
+console.log('Linha 2');
+/*
+console.log('Linha 3');
+console.log('Linha 4');
+*/
