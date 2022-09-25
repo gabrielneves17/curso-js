957bc15e2a6bc25b690cc3264e70b6c73bddd277
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-09-25 16:03:30 -0300

    Aula 10 - 25/09

---
+++ secao02/aula10/index.js
@@ -0,0 +1,17 @@
+// Tipos de dados primitivos
+
+const nome = 'Gabriel'; // String
+const nome1 = "Neves"; // String
+const nome2 = `Pieroni`; // String
+const num1 = 10; // Number
+const num2 = 10.52 // Number
+let nomeAluno; // Undefined = Não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo = Não aponta pra local nenhum na memória
+const aprovado = false; // Boolean = true, false(Lógico)
+
+let a = 2;
+let b = a;
+console.log(a, b); // 2, 2
+
+a = 3;
+console.log(a, b); // 3, 2
