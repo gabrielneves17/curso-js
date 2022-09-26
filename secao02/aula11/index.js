ec51d95f42e2f84f358b2274b9599180b50de3bc
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-09-26 20:23:15 -0300

    Aula 11 - 26/09

---
+++ secao02/aula11/index.js
@@ -0,0 +1,43 @@
+/* Operadores Aritméticos
+    
+    + Adição e Concatenação
+    - Subtração
+    * Multiplicação 
+    / Divisão
+    ** Potência
+    % Resto da divisão
+
+ Ordem de Precedência
+
+    1° ()
+    2° **
+    3° *, /, %
+    4° +, -
+
+ Incremento = ++
+    Pré-incremento vem antes da varíavel ou expressão 
+    Pos-incremento vem depois da varíavel ou expressão
+ Decremento = --
+    Pré-decremento vem antes da varíavel ou expressão 
+    Pos-decremento vem depois da varíavel ou expressão
+
+ Operadores de Atribuição
+    **=
+    *=
+    /=
+    %=
+    +=
+    -=
+
+ Tratamento ou conversão de dados
+
+    parseInt(); - Números inteiros
+    parseFloat(); - Números decimais
+    Number(); - Qualquer tipo de número
+    String(); - Converte para string
+*/
+
+const num1 = 10;
+const num2 = Number('5');
+console.log(num1 + num2);
+console.log(typeof(num2));
