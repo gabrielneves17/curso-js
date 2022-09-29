d82caf174810a2e0a363703065cbbbd15368e0f4
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-09-29 19:42:10 -0300

    Exercício e aulas 11 e 12 - 29/09

---
+++ secao02/aula11/script.js
@@ -0,0 +1,8 @@
+// alert, confirm, prompt
+
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+num1 = parseInt(num1);
+num2 = Number(num2);
+let resultado = num1 + num2
+alert(`O resultado da soma foi ${resultado}`);
---
+++ secao02/aula12/exvar.js
@@ -0,0 +1,9 @@
+// Exercício com variáveis
+
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC);
