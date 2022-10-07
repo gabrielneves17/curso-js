9df60a91f2a990f59083639be34b498355663835
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-10-06 22:28:36 -0300

    Atualizações aula 13 - 06/10

--- secao02/aula13/index.js
+++ secao02/aula13/index.js
@@ -1,8 +1,13 @@
-//Mais sobre Strings
-
-
-//                 0123456789
-let umaString = 'O rato roeu a roupa do rei de roma.';
-
-console.log(umaString.toUpperCase())
-console.log(umaString.toLowerCase())
+//               01234567
+let umaString = 'O rato roeu a roupa do rei de roma';
+console.log(umaString[2]); // Usa o índice para retornar o elemento indicado na variável de tipo string.
+console.log(umaString.charAt(2)); // Tem a mesma função do método acima, porém não retorna erro caso o índice indicado não exista.
+console.log(`${umaString} em um lindo dia.`); // Formas de concatenação
+console.log(umaString.concat(' em um lindo dia.'));
+console.log(umaString + ' em um lindo dia.');
+console.log(umaString.indexOf('t', 2)); //Retorna o índice de localização do valor armazenado. Caso seja um texto retorna o índice de início desse texto. Caso o valor procurado não exista retorna -1
+console.log(umaString.lastIndexOf('x', 6)); //Retorna o índice de localização do valor armazenado, porém de trás pra frente.
+console.log(umaString.match(/[a-z]/g)); //Expressão regular que retorna todas as letras minúsculas em forma de array.
+console.log(umaString.search(/o/)) //Retorna também o índice do valor deseja, mas sua diferença está na aceitação de expressões regulares.
+console.log(umaString.replace(/r/g, '#')) //Utiliza expressões regulares e também consegue alterar valores em variáveis.
+console.log(umaString.length); //Atributo que permite ver o comprimento ou tamanho de strings armazenadas.
