1269d5e07196606397ffd52d5376ee6b56e8b58c
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-10-08 20:58:04 -0300

    Atualização de aula 13 e exercício - 08/10

--- secao02/aula13/index.js
+++ secao02/aula13/index.js
@@ -1,5 +1,5 @@
 //               01234567
-let umaString = 'O rato roeu a roupa do rei de roma';
+let umaString = 'O rato roeu a roupa do rei de roma.';
 console.log(umaString[2]); // Usa o índice para retornar o elemento indicado na variável de tipo string.
 console.log(umaString.charAt(2)); // Tem a mesma função do método acima, porém não retorna erro caso o índice indicado não exista.
 console.log(`${umaString} em um lindo dia.`); // Formas de concatenação
@@ -11,3 +11,6 @@ console.log(umaString.match(/[a-z]/g)); //Expressão regular que retorna todas a
 console.log(umaString.search(/o/)) //Retorna também o índice do valor deseja, mas sua diferença está na aceitação de expressões regulares.
 console.log(umaString.replace(/r/g, '#')) //Utiliza expressões regulares e também consegue alterar valores em variáveis.
 console.log(umaString.length); //Atributo que permite ver o comprimento ou tamanho de strings armazenadas.
+console.log(umaString.slice(-5, -1)); //Faz o fatiamento de um trecho de texto usando o índice do ínicio e o índice do final do trecho.
+console.log(umaString.substring(umaString.length - 5, umaString.length - 1)); //Faz o fatiamento também.
+console.log(umaString.split(' ', 3)); //Cria um array com os elementos armazenados na varíavel.
