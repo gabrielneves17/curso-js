7ebc45b9e956bf3ba654eb45e9aaa2e0971ee3d4
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-10-15 21:38:49 -0300

    Aula 16 - 15/10

---
+++ secao02/aula16/index.js
@@ -0,0 +1,32 @@
+// Array(Basico)
+
+//                0        1        2
+const alunos = ['Luiz', 'Maria', 'João'];
+
+alunos.push('Gabriel');
+alunos.push('Larissa');
+console.log(alunos);
+
+// Pergunta se aquilo é um Array
+
+//console.log(alunos.slice(0, -2)); // Fatiamento de elementos em array
+
+//delete alunos[1]; //Apaga o elemento sem retirar o índice.
+
+//alunos.shift(); // Remove o elemento do ínicio e altera o índice dos demais elementos do array
+//alunos.pop(); // Remove o elemento do final junto ao seu índice
+
+//alunos.unshift('Luíza');
+//alunos.unshift('Fábio'); // Adiciona elementos no começo alterando a posição(índice) de outros.
+
+//alunos.push('Luiza'); // Adiciona no fim
+//alunos.push('Fábio');
+
+//alunos[alunos.length] = 'Luiza'; // Adiciona no fim
+//alunos[alunos.length] = 'Fábio';
+//alunos[alunos.length] = 'Luana';
+
+//console.log(alunos);
+//console.log(alunos[0]); // Acessando
+//console.log(alunos[2]);
+
