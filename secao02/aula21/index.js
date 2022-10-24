9217dba2d504f7bad373692f6cff7d766a65efe1
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-10-24 20:04:13 -0300

    Aula 21 - 24/10
    Operadores de comparação(Relacionais)

---
+++ secao02/aula21/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores de Comparação(Relacionais)
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+== Igual a (checa valor) *********** NÃO USAR
+=== Igualdade estrita (checa valor e tipo)
+!= Diferente de (checa valor) ********* NÃO USAR
+!== Diferente estrito (checa valor e tipo)
+*/
+const num1 = 10; // number
+const num2 = 10; // string
+const comp = num1 !== num2;
+console.log(comp);
