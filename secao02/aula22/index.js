b560181603bd1657f88161b84c0ca86781c506ac
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-10-25 20:28:48 -0300

    Aula 22 - 25/10

---
+++ secao02/aula22/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores Lógicos
+&& -> AND -> E (Todas as expressões do teste precisam ser verdadeiras para o resultado ser verdadeiro.)
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+//const expressaoAnd = true && true && false && true;
+//const expressaoOr = false || false || false || false;
+/*
+const usuario = 'Luiz'; // form que o usuário digitou
+const senha = '123456'; // form que o usuário digitou
+
+//                      true                false
+const vaiLogar = usuario === 'Luiz'  senha === '12345';
+*/
+
+console.log(!true); //Nega expressão e inverte o seu valor(Se era verdadeiro passa a ser falso e se era falso passa a ser verdadeiro)
