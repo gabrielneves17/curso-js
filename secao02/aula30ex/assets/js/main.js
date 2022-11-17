b454380e6870a9af4830c4320927d4b7b5081ded
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-11-16 21:30:04 -0300

    Atualizando exercício - 16/11
    Atualizando exercício da aula 30

--- secao02/aula30ex/assets/js/main.js
+++ secao02/aula30ex/assets/js/main.js
@@ -1,39 +1,38 @@
-const titulo = document.querySelector('.container h1');
+const h1 = document.querySelector('.container h1');
 const data = new Date();
+h1.innerHTML = data.toString();
 
-function pegaDia(diaSemana) {
-    const diasSemana = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'];
+function pegaDiaSem(diaSemana) {
+    const diaSemanaTexto = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'];
 
-    return diasSemana[diaSemana];
+    return diaSemanaTexto[diaSemana];
 }
 
 function pegaMes(numeroMes) {
-    const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+    const nomeMes = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
 
-    return meses[numeroMes];
-}
-
-function criaData(data) {
-    const diaSemana = data.getDay();
-    const numeroMes = data.getMonth();
-
-    const nomeDia = pegaDia(diaSemana);
-    const nomeMes = pegaMes(numeroMes);
-
-    return `<p>${nomeDia}, ${data.getDate()} de ${nomeMes} de ${data.getFullYear()} <p>${zeroEsquerda(data.getHours())}:${zeroEsquerda(data.getMinutes())}</p>`
+    return nomeMes[numeroMes];
 }
 
 function zeroEsquerda(num) {
     return num >= 10 ? num : `0${num}`
 }
 
-titulo.innerHTML = criaData(data);
+function criaData(data) {
+    const diaSemana = data.getDay();
+    const numeroMes = data.getMonth();
+
+    const nomeDia = pegaDiaSem(diaSemana);
+    const nomeMes = pegaMes(numeroMes);
+
+    return `<p>${nomeDia}, ${data.getDate()} de ${nomeMes} de ${data.getFullYear()}</p>
+    ${zeroEsquerda(data.getHours())}:${zeroEsquerda(data.getMinutes())}`
+}
+
+h1.innerHTML = criaData(data);
 
 /*
-const titulo = document.querySelector('.container h1');
+const h1 = document.querySelector('.container h1');
 const data = new Date();
-titulo.innerHTML = data.toLocaleDateString('pt-BR', { dateStyle: 'full', timeStyle: 'short' })
+h1.innerHTML = data.toLocaleString('pt-BR', {dateStyle: 'full', timeStyle: 'short'});
 */
-
-
-
