333b6cac3bfc64c6455c30aa1f905be11a7ce1ec
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-11-21 19:32:20 -0300

    Aula 34 - 21/11
    Estrutura de repetição (for)

---
+++ secao02/aula34/index.js
@@ -0,0 +1,15 @@
+// Estrutura de repetição (For)
+
+/*
+console.log('linha0');
+console.log('linha1');
+console.log('linha2');
+console.log('linha3');
+console.log('linha4');
+console.log('linha5');
+*/
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Manga', 'Pêssego'];
+for(let i = 0; i < frutas.length; i++) {
+    console.log(`Indice ${i}`, frutas[i]);
+}
