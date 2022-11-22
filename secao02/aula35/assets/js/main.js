1fff0da3ff30b7d4f060182155dd125f25523d3b
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-11-22 20:02:59 -0300

    Update main.js

--- secao02/aula35/assets/js/main.js
+++ secao02/aula35/assets/js/main.js
@@ -1,3 +1,4 @@
+/*
 const elementos = [
     {tag: 'p', texto: 'Frase 1'},
     {tag: 'div', texto: 'Frase 2'},
@@ -11,8 +12,27 @@ const div = document.createElement('div');
 for(let i = 0; i < elementos.length; i++) {
     const {tag, texto} = elementos[i];
     const elemCriado = document.createElement(tag);
-    const txt = document.createTextNode(texto);
-    elemCriado.appendChild(txt);
+    elemCriado.innerText = texto;
+    div.appendChild(elemCriado);
+}
+container.appendChild(div);
+*/
+
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'section', texto: 'Frase 3'},
+    {tag: 'footer', texto: 'Frase 4'}
+];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for(let i = 0; i < elementos.length; i++) {
+    const {tag, texto} = elementos[i];
+    const elemCriado = document.createElement(tag);
+    const textoCriado = document.createTextNode(texto);
+    elemCriado.appendChild(textoCriado);
     div.appendChild(elemCriado);
 }
 container.appendChild(div);
