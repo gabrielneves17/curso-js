93994df49895b58ebd4e1b5bde2f2a91aa802f08
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-11-24 19:41:53 -0300

    Update main.js

--- secao02/aula35/assets/js/main.js
+++ secao02/aula35/assets/js/main.js
@@ -22,7 +22,7 @@ const elementos = [
     {tag: 'p', texto: 'Frase 1'},
     {tag: 'div', texto: 'Frase 2'},
     {tag: 'section', texto: 'Frase 3'},
-    {tag: 'footer', texto: '4'}
+    {tag: 'footer', texto: 'Frase 4'}
 ];
 
 const container = document.querySelector('.container');
