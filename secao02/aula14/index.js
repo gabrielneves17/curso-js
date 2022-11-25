033da15cdc2aba78bafbbb160fe8e26e329367d7
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-11-24 21:04:54 -0300

    Update index.js

--- secao02/aula14/index.js
+++ secao02/aula14/index.js
@@ -1,7 +1,9 @@
 // IEEE 754-2008 Padrão
 
+
 let num1 = 0.7; // Number
 let num2 = 0.1; // Number
+
 num1 += num2; // 0.8
 num1 += num2; // 0.9
 num1 += num2; // 1.0
@@ -9,11 +11,11 @@ num1 = Number(num1.toFixed(2));
 console.log(num1);
 console.log(Number.isInteger(num1));
 
-/*
+
 console.log(num1.toString() + num2) 
 num1 = num1.toString();
-console.log(num1.toFixed(4));
+console.log(num1.toFixed(2));
 console.log(Number.isInteger(num1));
 let temp = num1 * '5';
 console.log(Number.isNaN(temp));
-*/
+
