050e278f26f2f2b83e0e8f8325d038ef87f66034
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-11-30 19:48:58 -0300

    Update main.js

--- secao02/aula38ex/assets/js/main.js
+++ secao02/aula38ex/assets/js/main.js
@@ -1,3 +1,4 @@
+/*
 const titulo = document.querySelector('.container');
 const paragrafos = document.querySelector('.paragrafos');
 const ps = paragrafos.querySelectorAll('p');
@@ -9,4 +10,17 @@ console.log(backgroundColorBody);
 for(let p of ps) {
     p.style.backgroundColor = backgroundColorBody;
     p.style.color = 'white';
+}
+*/
+
+const paragrafos = document.querySelector('.paragrafos');
+const p = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody);
+
+for(let valor of p) {
+    valor.style.backgroundColor = backgroundColorBody;
+    valor.style.color = '#ffffff';
 }
